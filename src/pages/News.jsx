a44191ec1a49a6af1d3706733383e32f67724c0b
UgurcanSARICI <ugurcansarici@gmail.com>
2022-11-14 00:45:54 +0300

    slice update

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -5,8 +5,17 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { CardMedia } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { getNews } from "../features/newsSlice";
+import { useEffect } from "react";
 
 const News = () => {
+  const { newsList } = useSelector((state) => state.news);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getNews());
+  }, []);
+
   return (
     <>
       <h1>NEWS</h1>
@@ -17,7 +26,7 @@ const News = () => {
         justifyContent="space-evenly"
         flexWrap="wrap"
       >
-        {[1, 2, 3].map((item, index) => (
+        {newsList?.map((item, index) => (
           <Card sx={{ maxWidth: 345, m: 5, maxHeight: 600 }} key={index}>
             <CardMedia
               component="img"
